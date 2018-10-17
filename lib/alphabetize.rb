8eeab9f91ead7c870d286f1bf53c52d0f50c580c
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-17 21:01:16 +0000

    Complete alphabetize_in_esperanto lab

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
 def alphabetize(arr)
-  # code here
+  
+  return arr.sort if arr.all?{|word|word[0] == arr.first[0]}
+     
+  esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  esp_hash = Hash.new
+  count = 0
+  esp_alph.each_char{|letter|esp_hash[letter] = count+=1}
+
+  hash = {}
+  arr.each do |word|
+    esp_hash.each do |key, value|
+      hash[word] = value if word[0] == key
+    end
+  end
+  
+  temp = hash.sort_by{|key, value| value}
+
+  result = []
+  temp.each{|word|result << word[0]}
+  result
 end
