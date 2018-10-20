72ebd115ed8ef4fbe195563f57ea5f72e6a6a1cc
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-20 05:28:49 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def alphabetize(arr)
   # code here
+  ea = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |esperanto_alphabet|
+    esperanto_alphabet.chars.collect do |i|
+      ea.chars.index(i)
+    end
+  end
 end
