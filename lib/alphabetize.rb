af8d45e78b1fe4fc79e7f6c1d5696c7da94c37d4
a lan <bluenowte97@gmail.com>
2018-11-18 20:38:09 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
 def alphabetize(arr)
   # code here
+  Esperanto_order = [a,b,c,ĉ,d,e,f,g,ĝ,h,ĥ,i,j,ĵ,k,l,m,n,o,p,r,s,ŝ,t,u,ŭ,v,z]
+  arr.sort_by do |x|
+    order.index(x[0] == x[0] ? 1:0, x)
+  end
 end
