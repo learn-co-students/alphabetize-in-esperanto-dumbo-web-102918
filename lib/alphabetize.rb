50dfc96634f7e1c0427eea4ab62c76b724c5766c
a lan <bluenowte97@gmail.com>
2018-11-18 21:33:00 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,10 @@
 def alphabetize(arr)
   # code here
-  Esperanto_order = [a,b,c,ĉ,d,e,f,g,ĝ,h,ĥ,i,j,ĵ,k,l,m,n,o,p,r,s,ŝ,t,u,ŭ,v,z]
-  arr.sort_by do |x|
-    order.index(x[0] == x[0] ? 1:0, x)
+  order = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |word|
+    #order.index(word[0])
+    word.split('').map do |letter|
+      order.index(letter)
+    end  
   end
 end
